7757b26c60637b40f7fabc60bc3ffa2601436e0e
elenarecio <74177148+elenarecio@users.noreply.github.com>
2024-06-15 12:58:56 +0200

    Crear estructura AcitividadAliada

--- Practica2C_2023/src/simulador_juego/solucion/ActividadAliada.java
+++ Practica2C_2023/src/simulador_juego/solucion/ActividadAliada.java
@@ -1,5 +1,16 @@
 package simulador_juego.solucion;
 
-public class ActividadAliada {
+public class ActividadAliada extends Thread{
+	private int tipoEnemigo;
+	private IJuego juego;
+	
+	public ActividadAliada(int tipoEnemigo, IJuego juego) {
+		this.tipoEnemigo=tipoEnemigo;
+		this.juego=juego;
+	}
+	
+	public void run() {
+		
+	}
 
 }
