2bff97f5a8ad79a664feaca865e5c86122da857f
elenarecio <74177148+elenarecio@users.noreply.github.com>
2024-06-15 12:59:40 +0200

    Crear estructura ActividadEnemiga

--- Practica2C_2023/src/simulador_juego/solucion/ActividadEnemiga.java
+++ Practica2C_2023/src/simulador_juego/solucion/ActividadEnemiga.java
@@ -1,5 +1,16 @@
 package simulador_juego.solucion;
 
 public class ActividadEnemiga {
+	private int tipoEnemigo;
+	private IJuego juego;
+	
+	public ActividadEnemiga(int tipoEnemigo, IJuego juego) {
+		this.tipoEnemigo=tipoEnemigo;
+		this.juego=juego;
+	}
+	
+	public void run() {
+		
+	}
 
 }
