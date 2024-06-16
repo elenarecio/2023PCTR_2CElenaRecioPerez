c2fdb4b82ba486b97ba41a23945cb919d53f729e
elenarecio <74177148+elenarecio@users.noreply.github.com>
2024-06-16 12:59:16 +0200

    Completar codigo de SistemaLanzador

--- Practica2C_2023/src/simulador_juego/solucion/SistemaLanzador.java
+++ Practica2C_2023/src/simulador_juego/solucion/SistemaLanzador.java
@@ -1,7 +1,14 @@
-package simulador_juego.solucion;
-
 public class SistemaLanzador {
-	public static void main(String[] args) {
-		
-	}
+    public static void main(String[] args) {
+        IJuego juego = Juego.getJuego();
+
+        // Crear y lanzar los hilos de enemigos y aliados
+        for (int i = 0; i < 4; i++) {
+            ActividadEnemiga enemiga = new ActividadEnemiga(i, juego);
+            new Thread (enemiga).start();
+            
+            ActividadAliada aliada = new ActividadAliada(i, juego);
+            new Thread (aliada).start();
+        }
+    }
 }
