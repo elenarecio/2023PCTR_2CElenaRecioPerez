d0ed0f04c17f208ce2c1a322434cca3709208716
elenarecio <74177148+elenarecio@users.noreply.github.com>
2024-06-16 11:48:27 +0200

    Terminar codigo y comentar IJuego

--- Practica2C_2023/src/simulador_juego/solucion/IJuego.java
+++ Practica2C_2023/src/simulador_juego/solucion/IJuego.java
@@ -1,7 +1,18 @@
 package simulador_juego.solucion;
-
+/**
+ * Interfaz relacionada con todos los demas archivos
+ * Define los metodos para generar y eliminar enemigos
+ * 
+ * @author Elena Recio Pérez
+ * @date 16/06/2024
+ * @version 2 Convocatoria
+ * 
+ * */
 public interface IJuego {
-	void generarEnemigo(int tipoEnemigo);
-	void eliminarenemigo(int tipoEnemigo);
+	//Con este metodo se genera el enemigo de un tipo
+	void generarEnemigo(int tipoEnemigo) throws InterruptedException;
+
+	//Con este método se elimina un enemigo de un tipo
+	void eliminarenemigo(int tipoEnemigo) throws InterruptedException;
 
 }
