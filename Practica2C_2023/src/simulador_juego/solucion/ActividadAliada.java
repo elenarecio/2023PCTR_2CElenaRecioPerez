cb71410e98478a1376576324a465569173419a87
elenarecio <74177148+elenarecio@users.noreply.github.com>
2024-06-16 11:24:49 +0200

    Completar codigo y comentarios de  ActividadAliada

--- Practica2C_2023/src/simulador_juego/solucion/ActividadAliada.java
+++ Practica2C_2023/src/simulador_juego/solucion/ActividadAliada.java
@@ -1,4 +1,6 @@
-package simulador_juego.solucion;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class ActividadAliada extends Thread{
 	private int tipoEnemigo;
@@ -10,7 +12,17 @@ public class ActividadAliada extends Thread{
 	}
 	
 	public void run() {
-		
+		//Creamos el hilo para generar enemigos
+		while(!interrupted()) {
+			try {
+				//Tiempo de espera entre 1 y 5s y se genera el enemigo de un tipo
+				TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5)*1000);
+				juego.generarEnemigo(tipoEnemigo);
+			} catch (InterruptedException e) {
+				//finalizaria la ejecucion del hilo
+				break;
+			}
+		}
 	}
-
 }
+
